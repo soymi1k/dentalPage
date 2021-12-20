0d1de2b231b689896719501b59d658aba0928742
soymi1k <soymilkneigh@gmail.com>
2021-12-20 13:05:30 +0900

    add map marker

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var mapOptions = {
+var map = new naver.maps.Map("map", {
   center: new naver.maps.LatLng(37.4816620985679, 126.95323042623738),
-  zoom: 10,
-};
+  zoom: 17,
+});
 
-var map = new naver.maps.Map("map", mapOptions);
+var marker = new naver.maps.Marker({
+  position: new naver.maps.LatLng(37.4816620985679, 126.95323042623738),
+  map: map,
+});
