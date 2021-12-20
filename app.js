5b45bc0ee338865d09d97d867359684c0a66edc1
soymi1k <soymilkneigh@gmail.com>
2021-12-20 13:19:17 +0900

    add map marker02

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var map = new naver.maps.Map("map", {
-  center: new naver.maps.LatLng(37.4816620985679, 126.95323042623738),
-  zoom: 17,
+  center: new naver.maps.LatLng(37.481480203672376, 126.95321693593108),
+  zoom: 18,
 });
 
 var marker = new naver.maps.Marker({
-  position: new naver.maps.LatLng(37.4816620985679, 126.95323042623738),
+  position: new naver.maps.LatLng(37.481480203672376, 126.95321693593108),
   map: map,
 });
