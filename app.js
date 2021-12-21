ed44f98b967f7c96f6a09d230eeb091b816f1b0d
soymi1k <soymilkneigh@gmail.com>
2021-12-21 20:39:48 +0900

    add map option

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var map = new naver.maps.Map("map", {
   center: new naver.maps.LatLng(37.481480203672376, 126.95321693593108),
+  disableDoubleTapZoom: true,
+  draggable: false,
+  keyboardShortcuts: false,
   zoom: 18,
+  zoomOrigin: new naver.maps.LatLng(37.481480203672376, 126.95321693593108),
 });
 
 var marker = new naver.maps.Marker({
