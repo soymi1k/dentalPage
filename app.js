01e6447a6c174bfdfc56bf2197f0c3a6afcc6e25
soymi1k <soymilkneigh@gmail.com>
2021-12-27 11:49:58 +0900

    make out layout reactivly

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var map = new naver.maps.Map("map", {
   center: new naver.maps.LatLng(37.481480203672376, 126.95321693593108),
-  disableDoubleTapZoom: true,
+  // disableDoubleTapZoom: true,
   draggable: false,
   keyboardShortcuts: false,
   zoom: 18,
