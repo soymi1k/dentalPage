afd917fadc758ac23827baa04db888e6e96fea81
soymi1k <soymilkneigh@gmail.com>
2022-01-03 19:06:42 +0900

    add nav and kakao abstract

--- app.js
+++ app.js
@@ -11,3 +11,10 @@ var marker = new naver.maps.Marker({
   position: new naver.maps.LatLng(37.481480203672376, 126.95321693593108),
   map: map,
 });
+
+const toggleBtn = document.querySelector(".toggleBtn");
+const menu = document.querySelector(".menuBox");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
